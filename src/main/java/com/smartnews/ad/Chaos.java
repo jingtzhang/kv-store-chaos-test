846500b7530572f6483cf78a3f67b00c6a25b470
jtzhang <jingtong.zhang@smartnews.com>
2021-07-20 17:51:43 +0800

    try

--- src/main/java/com/smartnews/ad/Chaos.java
+++ src/main/java/com/smartnews/ad/Chaos.java
@@ -103,7 +103,7 @@ public class Chaos {
     }
 
     public void keepQuerying(int batchSize, int intervalNum, int interval, int threadNum) throws InterruptedException {
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(threadNum, 8, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(500000), new DiscardOldestPolicyImpl());
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(threadNum, 20, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(500000), new DiscardOldestPolicyImpl());
 
         AtomicLong seq = new AtomicLong();
         Random random = new Random(System.currentTimeMillis());
--- src/test/java/com/smartnews/ad/ChaosTest.java
+++ src/test/java/com/smartnews/ad/ChaosTest.java
@@ -24,7 +24,7 @@ public class ChaosTest {
 
     @Test
     public void query() throws InterruptedException {
-        chaos.keepQuerying(100, 500, 200, 4);
+        chaos.keepQuerying(100, 500, 200, 16);
     }
 
     @Test
