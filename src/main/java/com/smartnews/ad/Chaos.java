109084ef2e330dc0ee6fde0687023e61cb2630e7
jtzhang <jingtong.zhang@smartnews.com>
2021-07-20 16:47:36 +0800

    fix

--- src/main/java/com/smartnews/ad/Chaos.java
+++ src/main/java/com/smartnews/ad/Chaos.java
@@ -160,12 +160,4 @@ public class Chaos {
             }
         }
     }
-
-    public static void main(String[] args) throws KvStoreClient.ClientInitializationException, InterruptedException {
-        KvStoreClient kvStoreClient = new KvStoreClient("jingtong_test",
-                "kv-stg-read-proxy-nlb.dynamic-ads.smartnews.net", 9000,
-                "kv-stg-write-proxy-nlb.dynamic-ads.smartnews.net", 9001);
-        Chaos chaos = new Chaos(kvStoreClient, 60);
-        chaos.keepQuerying(50, 500, 0, 4);
-    }
 }
