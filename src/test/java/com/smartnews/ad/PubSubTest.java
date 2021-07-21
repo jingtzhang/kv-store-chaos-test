34ff0412da220e6f0a83e424c6c9c4c7f633518e
jtzhang <jingtong.zhang@smartnews.com>
2021-07-21 16:49:03 +0800

    test conn

--- src/test/java/com/smartnews/ad/PubSubTest.java
+++ src/test/java/com/smartnews/ad/PubSubTest.java
@@ -1,4 +1,7 @@
 package com.smartnews.ad;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.api.model.Endpoints;
 import io.lettuce.core.*;
 import io.lettuce.core.cluster.RedisClusterClient;
 import io.lettuce.core.cluster.models.partitions.RedisClusterNode;
@@ -19,11 +22,29 @@ import static java.lang.Thread.sleep;
 
 public class PubSubTest {
 
+    KubernetesClient k8sClient;
     RedisClusterClient clusterClient;
     StatefulRedisClusterPubSubConnection<String, String> connection;
 
     @Before
     public void setUp() {
+//        k8sClient = new DefaultKubernetesClient();
+//        Endpoints etcd = k8sClient.endpoints().inNamespace("dynamic-ads").withName("etcd").get();
+//        System.out.println(endpoints.getSubsets().get(0).getAddresses().get(0).getIp());
+//        Service myservice = k8sClient.services().inNamespace("dynamic-ads").withName("etcd-client").get();
+//        System.out.println(myservice);
+//        String etcdHostName = myservice.getStatus().getLoadBalancer().getIngress().get(0).getHostname();
+//        Integer port = myservice.getSpec().getPorts().get(0).getPort();
+//        System.out.println(etcdHostName+":"+port);
+//        Endpoints redisEndpoints = k8sClient.endpoints().inNamespace("dynamic-ads").withName("redis-cluster-headless").get();
+//        List<String> redisSeeds = new ArrayList<>();
+//        redisSeeds.add(redisEndpoints.getSubsets().get(0).getAddresses().get(0).getIp());
+//        redisSeeds.add(redisEndpoints.getSubsets().get(0).getAddresses().get(1).getIp());
+//        redisSeeds.add(redisEndpoints.getSubsets().get(0).getAddresses().get(2).getIp());
+//        RedisURI node1 = RedisURI.create(redisSeeds.get(0), 6379);
+//        RedisURI node2 = RedisURI.create(redisSeeds.get(1), 6379);
+//        RedisURI node3 = RedisURI.create(redisSeeds.get(2), 6379);
+
         List<RedisURI> uris = new ArrayList<RedisURI>(){{
             add(RedisURI.create("10.1.131.101", 6379));
             add(RedisURI.create("10.1.129.21", 6379));
@@ -72,9 +93,9 @@ public class PubSubTest {
         RedisPubSubCommands<String, String> sync = connection.sync();
         sync.subscribe("test-channel");
 
-        RedisClusterPubSubAsyncCommands<String, String> async = connection.async();
+//        RedisClusterPubSubAsyncCommands<String, String> async = connection.async();
         while(true) {
-            async.publish("test-channel", UUID.randomUUID().toString());
+//            async.publish("test-channel", UUID.randomUUID().toString());
             sleep(2000);
         }
     }
