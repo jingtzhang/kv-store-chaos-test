e204197016797e2ca5f7353eb7752520cab5cfa6
jtzhang <jingtong.zhang@smartnews.com>
2021-07-22 14:32:18 +0800

    refactor

--- src/test/java/com/smartnews/ad/PubSubTest.java
+++ src/test/java/com/smartnews/ad/PubSubTest.java
@@ -1,6 +1,5 @@
 package com.smartnews.ad;
 import io.fabric8.kubernetes.api.model.EndpointAddress;
-import io.fabric8.kubernetes.api.model.Service;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.api.model.Endpoints;
@@ -10,6 +9,7 @@ import io.lettuce.core.cluster.models.partitions.RedisClusterNode;
 import io.lettuce.core.cluster.pubsub.RedisClusterPubSubListener;
 import io.lettuce.core.cluster.pubsub.StatefulRedisClusterPubSubConnection;
 import io.lettuce.core.cluster.pubsub.api.async.RedisClusterPubSubAsyncCommands;
+import io.lettuce.core.cluster.pubsub.api.sync.RedisClusterPubSubCommands;
 import io.lettuce.core.pubsub.api.sync.RedisPubSubCommands;
 import org.junit.After;
 import org.junit.Before;
@@ -25,50 +25,34 @@ import static java.lang.Thread.sleep;
 public class PubSubTest {
 
     KubernetesClient k8sClient;
-    RedisClusterClient clusterClient;
-    RedisClusterClient asyncClient;
     StatefulRedisClusterPubSubConnection<String, String> connection;
     StatefulRedisClusterPubSubConnection<String, String> asyncConnection;
+    RedisClusterPubSubCommands<String, String> sync;
+    RedisClusterPubSubAsyncCommands<String, String> async;
 
 
     @Before
     public void setUp() {
+        // Retrieve IP Addresses of Redis Cluster Pods
         k8sClient = new DefaultKubernetesClient();
-//        Endpoints endpoints = k8sClient.endpoints().inNamespace("dynamic-ads").withName("etcd").get();
-//        System.out.println(endpoints.getSubsets().get(0).getAddresses().get(0).getIp());
-//        Service myService = k8sClient.services().inNamespace("dynamic-ads").withName("etcd-client").get();
-//        System.out.println(myService);
-//        String etcdHostName = myService.getStatus().getLoadBalancer().getIngress().get(0).getHostname();
-//        Integer port = myService.getSpec().getPorts().get(0).getPort();
-//        System.out.println(etcdHostName+":"+port);
         Endpoints redisEndpoints = k8sClient.endpoints().inNamespace("dynamic-ads").withName("redis-cluster-stg-headless").get();
         List<EndpointAddress> addresses = redisEndpoints.getSubsets().get(0).getAddresses();
-        addresses.forEach((address) ->
-            System.out.println(address.getIp()));
-//        List<String> redisSeeds = new ArrayList<>();
-//        redisSeeds.add(redisEndpoints.getSubsets().get(0).getAddresses().get(0).getIp());
-//        redisSeeds.add(redisEndpoints.getSubsets().get(0).getAddresses().get(1).getIp());
-//        redisSeeds.add(redisEndpoints.getSubsets().get(0).getAddresses().get(2).getIp());
-//        RedisURI node1 = RedisURI.create(redisSeeds.get(0), 6379);
-//        RedisURI node2 = RedisURI.create(redisSeeds.get(1), 6379);
-//        RedisURI node3 = RedisURI.create(redisSeeds.get(2), 6379);
+        List<RedisURI> uris = new ArrayList<>();
+        System.out.println("Redis Cluster IP Addresses:");
+        addresses.forEach((address) -> {
+            System.out.println(address.getIp());
+            uris.add(RedisURI.create(address.getIp(), 6379));
+        });
 
-//        List<RedisURI> uris = new ArrayList<RedisURI>(){{
-//            add(RedisURI.create("10.1.131.101", 6379));
-//            add(RedisURI.create("10.1.129.21", 6379));
-//            add(RedisURI.create("10.1.131.39", 6379));
-//            add(RedisURI.create("10.1.128.179", 6379));
-//            add(RedisURI.create("10.1.131.181", 6379));
-//            add(RedisURI.create("10.1.129.233", 6379));
-//        }};
-//        clusterClient = RedisClusterClient.create(uris);
-//        asyncClient = RedisClusterClient.create(uris);
-//        connection = clusterClient.connectPubSub();
-//        asyncConnection = asyncClient.connectPubSub();
-    }
+        // Cannot subscribe and publish at the same connection!
+        // Create a connection for subscriber
+        connection = RedisClusterClient.create(uris).connectPubSub();
+        sync = connection.sync();
 
-    @Test
-    public void test() throws InterruptedException, ExecutionException {
+        // Create a connection for publisher
+        asyncConnection = RedisClusterClient.create(uris).connectPubSub();
+        async = asyncConnection.async();
+
         connection.addListener(new RedisClusterPubSubListener<String, String>() {
             @Override
             public void message(RedisClusterNode redisClusterNode, String s, String s2) {
@@ -100,10 +84,12 @@ public class PubSubTest {
 
             }
         });
-        RedisPubSubCommands<String, String> sync = connection.sync();
+    }
+
+    @Test
+    public void test() throws InterruptedException, ExecutionException {
         sync.subscribe("test-channel");
 
-        RedisClusterPubSubAsyncCommands<String, String> async = asyncConnection.async();
         while(true) {
             async.publish("test-channel", UUID.randomUUID().toString()).get();
             sleep(2000);
@@ -113,6 +99,7 @@ public class PubSubTest {
     @After
     public void tearDown() {
         connection.close();
-        clusterClient.shutdown();
+        asyncConnection.close();
+        k8sClient.close();
     }
 }
